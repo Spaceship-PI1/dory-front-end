f16bdd4f7317d3344139924f1ddb8541f9afb993
Larissa Araújo <lariaraujo201@gmail.com>
2022-02-02 19:08:34 -0300

    add menu nav global

--- src/components/NavBarGlobal/index.js
+++ src/components/NavBarGlobal/index.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
+import Box from "@mui/material/Box";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Avatar from '@mui/material/Avatar';
 
 import logo from '../../assets/logo-negativa.svg';
 import envelope from '../../assets/icons/envelope.svg';
@@ -8,6 +14,14 @@ import imgPerfil from '../../assets/perfil.png';
 import './style.css';
 
 export default function NavBarGlobal() {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (e) => setAnchorEl(e.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+
+    const perfil = "aluno";
+
     return (
         <nav className="container">
             <div className="div-logo">
@@ -39,17 +53,65 @@ export default function NavBarGlobal() {
                         Solicitações
                     </Link>
                 </div>
-                <div className="item">
-                    <div className="circle-img">
-                        <img className="perfil-icon" src={imgPerfil} alt="Foto de perfil de Alissa" />
-                    </div>
-                    
-                    <Link className="access"
-                        to={() => {}}
+
+                <Box 
+                    className="item"
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        marginLeft: '20px'
+                    }}
+                >   
+                    <Avatar 
+                        alt="Cindy Baker" 
+                        src={imgPerfil} 
+                        sx={{ width: 32, height: 32, marginTop: 1 }}
+                    />
+
+                    <Button
+                        className="access"
+                        id="basic-button"
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                        sx={{
+                            textTransform: 'none',
+                            fontFamily: 'Roboto',
+                            fontWeight: 500
+                        }}
                     >
                         Alissa
-                    </Link>
-                </div>
+                    </Button>
+
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                    {perfil === "professor" ?
+                        <>
+                            <MenuItem onClick={() => {}} sx={{fontSize: '1.4rem'}}>Ver Perfil</MenuItem>
+                            <MenuItem onClick={() => {}} sx={{fontSize: '1.4rem'}}>Ver "Meus TCCs"</MenuItem>
+                            <MenuItem onClick={() => {}} sx={{fontSize: '1.4rem'}}>Ver Solicitações</MenuItem>
+                            <MenuItem onClick={() => {}} sx={{fontSize: '1.4rem'}}>Atualizar Disponibilidade</MenuItem>
+                            <MenuItem onClick={() => {}} sx={{fontSize: '1.4rem'}}>Sair</MenuItem>
+                        </>
+                    :
+                        <>
+                            <MenuItem onClick={() => {}} sx={{fontSize: '1.4rem'}}>Ver Perfil</MenuItem>
+                            <MenuItem onClick={() => {}} sx={{fontSize: '1.4rem'}}>Ver Solicitações</MenuItem>
+                            <MenuItem onClick={() => {}} sx={{fontSize: '1.4rem'}}>Sair</MenuItem>
+                        </> 
+                    }
+                    </Menu>
+                </Box>
             </div>
         </nav>
     )
