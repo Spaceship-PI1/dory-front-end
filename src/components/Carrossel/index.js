69757fe98551978ef79186150eee842dc36d68cd
Larissa Araújo <lariaraujo201@gmail.com>
2022-02-03 14:31:44 -0300

    update home

---
+++ src/components/Carrossel/index.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import NavFiltrosContextuais from "../NavFiltrosContextuais";
+import CardGroup from "../CardGroup";
+import ButtonNav from "../ButtonNav";
+
+import './style.css';
+
+export default function Carrossel({ index, interesse }) {
+    const [status, setStatus] = useState("professores");
+
+    const handleSetProfessores = () => setStatus("professores");
+    const handleSetTCCs = () => setStatus("TCCs");
+
+    const srcPerfil = "https://images.pexels.com/photos/7163364/pexels-photo-7163364.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+
+    const listProfessores = [
+        {
+            id: 0,
+            foto: srcPerfil,
+            nome: "Alysson Diniz",
+            email: "alysson@virtual.ufc.br",
+            areas: [
+                'Realidade Virtual', 'Game design', 'Desenvolvimento de jogos', 'Jogos'
+            ],
+            status: "disponível" 
+        },
+        {
+            id: 1,
+            foto: srcPerfil,
+            nome: "Alysson Diniz",
+            email: "alysson@virtual.ufc.br",
+            areas: [
+                'Realidade Virtual', 'Game design', 'Desenvolvimento de jogos', 'Jogos'
+            ],
+            status: "disponível" 
+        },
+        {
+            id: 2,
+            foto: srcPerfil,
+            nome: "Alysson Diniz",
+            email: "alysson@virtual.ufc.br",
+            areas: [
+                'Realidade Virtual', 'Game design', 'Desenvolvimento de jogos', 'Jogos'
+            ],
+            status: "disponível" 
+        }
+    ]
+
+    const listTCCs = [
+        {
+            id: 0,
+            title: "Desenvolvimento de uma personagem para um projeto de jogo digital de terror utilizando a abordagem The Silver Way",
+            aluno: "Douglas Alves da Silva",
+            professor: "Liandro Roger", 
+            areas: [
+                'Design de personagens', 'Cartum', 'Jogos de Terror'
+            ]
+        },
+        {
+            id: 1,
+            title: "Análise de metodologias ágeis como recursos para desenvolvimento de jogos digitais de pequeno escopo",
+            aluno: "Henrique Artur Cordeiro Gomes",
+            professor: "Henrique Barbosa Silva", 
+            areas: [
+                'Metodologias ágeis', 'Jogos', 'Scrum'
+            ] 
+        },
+    ]
+
+    return (
+        <div key={index} className="item-resultado">
+            <h1>{interesse}</h1>
+
+            <div className="nav-filtros">
+                <div className="filtros"> 
+                    <ButtonNav 
+                        setStatus={handleSetProfessores}
+                        name="Professores"
+                        id={status == "professores" ? "selected" : null}
+                    />
+
+                    <ButtonNav 
+                        setStatus={handleSetTCCs}
+                        name="TCCs"
+                        id={status == "TCCs" ? "selected" : null}
+                    />
+                </div>
+                
+                <hr className="line" /> 
+
+                <NavFiltrosContextuais status={status} />
+            </div>
+
+            <CardGroup 
+            status={status}
+            list={status == "TCCs" ? listTCCs : listProfessores}
+        />
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,81 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 
 import NavBarGlobal from "../components/NavBarGlobal";
-import ButtonNav from "../components/ButtonNav";
-import CardGroup from "../components/CardGroup";
-import NavFiltrosContextuais from "../components/NavFiltrosContextuais";
+import Carrossel from "../components/Carrossel";
 
 import search from '../assets/icons/search-gray.svg';
  
 import '../styles/home.css';
 
 export function Home() {
-    const [status, setStatus] = useState("professores");
-
-    const handleSetProfessores = () => setStatus("professores");
-    const handleSetTCCs = () => setStatus("TCCs");
-
     const user = {
         interesses : [
             'Jogos', 'Design', 'Criação de personagens', 'Ilustração', 'Desenho' 
         ]
     }
-
-    const srcPerfil = "https://images.pexels.com/photos/7163364/pexels-photo-7163364.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
-
-    const listProfessores = [
-        {
-            id: 0,
-            foto: srcPerfil,
-            nome: "Alysson Diniz",
-            email: "alysson@virtual.ufc.br",
-            areas: [
-                'Realidade Virtual', 'Game design', 'Desenvolvimento de jogos', 'Jogos'
-            ],
-            status: "disponível" 
-        },
-        {
-            id: 1,
-            foto: srcPerfil,
-            nome: "Alysson Diniz",
-            email: "alysson@virtual.ufc.br",
-            areas: [
-                'Realidade Virtual', 'Game design', 'Desenvolvimento de jogos', 'Jogos'
-            ],
-            status: "disponível" 
-        },
-        {
-            id: 2,
-            foto: srcPerfil,
-            nome: "Alysson Diniz",
-            email: "alysson@virtual.ufc.br",
-            areas: [
-                'Realidade Virtual', 'Game design', 'Desenvolvimento de jogos', 'Jogos'
-            ],
-            status: "disponível" 
-        }
-    ]
-
-    const listTCCs = [
-        {
-            id: 0,
-            title: "Desenvolvimento de uma personagem para um projeto de jogo digital de terror utilizando a abordagem The Silver Way",
-            aluno: "Douglas Alves da Silva",
-            professor: "Liandro Roger", 
-            areas: [
-                'Design de personagens', 'Cartum', 'Jogos de Terror'
-            ]
-        },
-        {
-            id: 1,
-            title: "Análise de metodologias ágeis como recursos para desenvolvimento de jogos digitais de pequeno escopo",
-            aluno: "Henrique Artur Cordeiro Gomes",
-            professor: "Henrique Barbosa Silva", 
-            areas: [
-                'Metodologias ágeis', 'Jogos', 'Scrum'
-            ] 
-        },
-    ]
 
     return (
         <div>
@@ -104,34 +41,7 @@ export function Home() {
 
                 <div className="resultados-container">
                     {user.interesses.map((inter, index) => (
-                        <div key={index} className="item-resultado">
-                            <h1>{inter}</h1>
-
-                            <div className="nav-filtros">
-                                <div className="filtros"> 
-                                    <ButtonNav 
-                                        setStatus={handleSetProfessores}
-                                        name="Professores"
-                                        id={status == "professores" ? "selected" : null}
-                                    />
-
-                                    <ButtonNav 
-                                        setStatus={handleSetTCCs}
-                                        name="TCCs"
-                                        id={status == "TCCs" ? "selected" : null}
-                                    />
-                                </div>
-                                
-                                <hr className="line" /> 
-
-                                <NavFiltrosContextuais status={status} />
-                            </div>
-
-                            <CardGroup 
-                            status={status}
-                            list={status == "TCCs" ? listTCCs : listProfessores}
-                        />
-                        </div>
+                       <Carrossel index={index} interesse={inter} />
                     ))}
                 </div>
             </section>
