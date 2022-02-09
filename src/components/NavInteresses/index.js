d3af0f3c38d0239d251dfbd55a0f1670e3c85f13
Larissa Araújo <lariaraujo201@gmail.com>
2022-02-09 16:17:53 -0300

    add api in nav interesses

--- src/components/NavInteresses/index.js
+++ src/components/NavInteresses/index.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 import './style.css';
 
 export default function NavInteresses({ data }) {
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        navigate({
+            pathname: '/pesquisa',
+            search: `?q=${e.target.value}`
+        });
+
+        e.preventDefault();
+    }
+
     return (
         <div className="interesses-container">
             <p>Seus interesses</p>
             <ul className="interesses-list-tags">
-                { data?.map((inter, index) => <li key={index}>{inter}</li>)}
+                { data?.map((inter, index) => (
+                    <button key={index} onClick={handleSubmit} value={inter}>{inter}</button>))
+                }
             </ul>
         </div>
     )
